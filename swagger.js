258f19be9e308f3f5e7090a1a86b6e7c51dabc38
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-10-01 11:26:57 -0500

    Initialize server and set up basic middleware; add Swagger documentation generation

---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Movies API',
+    description: 'API for managing movies'
+  },
+  host: 'localhost:5000',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger.json'; // Output file for the generated Swagger documentation should be created in the root directory EMPTY
+const routes = ['./routes/index.js'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
