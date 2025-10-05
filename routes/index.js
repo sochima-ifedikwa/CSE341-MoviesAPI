acd1f661b2d62a0c0d1ce739bb6269b386cbc3e3
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-10-05 10:19:04 -0500

    Updataed the Welcome page

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,11 @@ router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
     //#swagger.tags = ['Welcome to the Movies API']
-    res.send('Hello World');  
+    res.send('Hello World and welcome to tour Movies API!');
+    res.send('<h1>Welcome to the MMovies API</h1><p>Use the /actors endpoint to manage actors collection.</p><p>Use the /directors endpoint to manage directors collection.</p><p>Use the /genres endpoint to manage genres collection.</p><p>Use the /movies endpoint to manage movies collection.</p>');
+    res.send('<h2>API Documentation</h2><p>Visit <a href="/api-docs">/api-docs</a> for the Swagger UI documentation.</p>');
+    res.status(200);
+
 });
 
 module.exports = router;
